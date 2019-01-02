ec30bda337b10180fde07b4b414f783106143e7d
sayid <herq12315@gmail.com>
2019-01-02 15:25:36 +0800

    first commit project setup

---
+++ README.md
@@ -0,0 +1,68 @@
+# How to set up the project
+
+## 1. Assets
+
+Yarn
+- https://yarnpkg.com/
+
+Gulp 4.0.0 Info
+- https://fettblog.eu/gulp-4-parallel-and-series/
+
+Google Fonts
+- https://fonts.google.com
+
+
+## 2. Structure
+
+目录结构为:
+
+```
+.
+├── .gitignore
+├── README.md
+├── assets
+├── dist
+├── gulpfile.js
+├── index.html
+├── package.json
+├── scripts
+├── stylesheets
+└── yarn.lock
+```
+
+`.gitignore` 文件中放置需要忽略的目录或文件
+`assets` 中是资料目录可以放置 `fonts` `images` 等
+`dists` 中是 gulp 生成的目标目录  
+`gulpfile.js` 是 gulp 的代码，可进行编辑定制 gulp 的自动化流程
+`package.json` 里是 gulpfile.js 中用到的 package
+`scripts` 中放置 js 代码
+`stylesheets` 中放置 SCSS 代码
+
+## 3. Set up environment
+
+```
+Dependencies: Nodejs, yarn
+```
+
+Install yarn
+```
+brew install yarn
+```
+
+Install packages
+```
+yarn install
+yarn add packagename
+```
+
+### 4. Run the project
+
+Run the gulp for development
+```
+gulp serve
+```
+
+Run the gulp for production, will compress the css file
+```
+gulp production
+```
---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+//Gulp Packages
+const gulp = require('gulp');
+const gulpIf = require('gulp-if');
+const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
+
+// const pug = require("gulp-pug");
+
+const imagemin = require('gulp-imagemin');
+
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require('gulp-sourcemaps');
+
+const ts = require("gulp-typescript");
+const tslint = require("gulp-tslint");
+const uglify = require('gulp-uglify');
+
+const jasmine = require("gulp-jasmine-phantom");
+
+const notify = require('gulp-notify');
+
+//Global Variables
+var production = false;
+
+//Path Definitions
+const paths = {
+    html: {
+        src: "*.html",
+        dest: "dist"
+    },
+    styles: {
+        main_scss_src: "stylesheets/**/main.scss",
+        src: "stylesheets/**/*.scss",
+        dest: "dist/stylesheets"
+    },
+    scripts: {
+        src: "scripts/**/*.js",
+        dest: "dist/scripts"
+    },
+    images: {
+        src: "images/**/*",
+        dest: "dist/images"
+    }
+}
+
+//Internal Tasks
+function cleanDist() {
+    return gulp.src('dist/')
+        .pipe(clean())
+}
+
+function html() {
+    return gulp.src(paths.html.src)
+//        .pipe(pug())
+        .pipe(gulp.dest(paths.html.dest))
+}
+
+function scripts () {
+    return gulp.src(paths.scripts.src)
+        .pipe(ts({
+            out: "output.js"
+        }))
+        .pipe(tslint({
+            formatter: "verbose"
+        }))
+        .pipe(tslint.report())
+        .pipe(gulpIf(production, uglify()))
+        .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function styles() {
+    let sassOptions = {};
+    if (production) {
+        sassOptions = {
+            outputStyle: 'compressed'
+        }
+    }
+    return gulp.src(paths.styles.main_scss_src)
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream())
+}
+
+function images () {
+    return gulp.src(paths.images.src)
+        .pipe(gulpIf(production, imagemin()))
+        .pipe(gulp.dest(paths.images.dest));
+}
+
+//External Tasks
+
+gulp.task("tests", () => {
+    return gulp.src("./tests/test.js")
+        .pipe(jasmine({
+            integration: true,
+            vendor: '_build/**/*.js'
+        }));
+})
+
+gulp.task("default",
+    gulp.series(cleanDist, gulp.parallel(html, scripts, styles, images))
+);
+
+gulp.task('production', gulp.series((done) => { production = true; done(); }, 'default'));
+
+gulp.task("serve", gulp.series('default', () => {
+    browserSync.init({
+        server: "./dist"
+    });
+    gulp.watch(paths.styles.src, gulp.series(styles));
+    gulp.watch(paths.scripts.src,  gulp.series(scripts));
+    gulp.watch(paths.images.src, gulp.series(images));
+    gulp.watch(paths.html.src, gulp.series(html)).on('change', browserSync.reload);
+}));
